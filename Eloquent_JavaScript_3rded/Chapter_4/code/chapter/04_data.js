4c27121931558e6366172725a32a27e823ebda21
Astralpuppy <rodrigonev98@gmail.com>
2022-01-04 14:50:47 -0700

    Prepared some pseudocode for solving exercises.

--- Eloquent_JavaScript_3rded/Chapter_4/code/chapter/04_data.js
+++ Eloquent_JavaScript_3rded/Chapter_4/code/chapter/04_data.js
@@ -55,3 +55,10 @@ var list = {
     }
   }
 };
+
+function remove(array, index) {
+  return array.slice(0, index)
+    .concat(array.slice(index + 1));
+}
+// console.log(remove(["a", "b", "c", "d", "e"], 2));
+// → ["a", "b", "d", "e"]
